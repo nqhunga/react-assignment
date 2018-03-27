1c155d15481ad30a41ba69660093ef6db5eb649a
nqhunga <nguyenquochung1989vt@gmail.com>
2018-03-27 15:20:33 +0300

    Updates

--- src/App.style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { Container } from 'reactstrap';
-
-export const ContainerEx = styled(Container)`
- 
-`;
--- src/database/database.js
+++
@@ -1,7 +0,0 @@
-export async function getUsers() {
-  const response = await fetch("https://randomuser.me/api/?results=20");
-
-  const responseData = response.json();
-
-  return responseData;
-}
